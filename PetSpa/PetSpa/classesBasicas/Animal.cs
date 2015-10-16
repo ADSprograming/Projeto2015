c772601e72a2e964b606175367960d6a9de2f8c8
unknown <aluno@pc-593.EXPERT.LOCAL>
2015-10-16 20:44:14 -0300

    Sistema com classes basicas

---
+++ PetSpa/PetSpa/classesBasicas/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesBasicas
+{
+    class Animal
+    {
+        String nome, raca,pet;
+        double peso,idade;
+        Dono dono;
+    }
+}
---
+++ PetSpa/PetSpa/classesBasicas/Dono.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesBasicas
+{
+    class Dono
+    {
+        String nome;
+        String cpf;
+        String telefone;
+        Endereco endereco;
+    }
+}
